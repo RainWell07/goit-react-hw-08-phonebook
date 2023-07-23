a28d46faf922e8860e6d3107529720a9a8fbb33b
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-07-23 18:28:24 +0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ export default function Home() {
     return(
         <div className={css.homeOverlay}>
             <h1 className={`${css.homeContainer} ${css.basicFont}`}>Welcome👋</h1>
-            <h2 className={`${css.homeContainer} ${css.basicFontHome}`} >Just use <span style={{fontWeight:'bold'}}>Log In</span> or <span style={{fontWeight:'bold'}}>Registration</span> to your personal <span style={{fontWeight:'bolder'}}>Phone</span><span className={css.bookLogo}>Book</span></h2>
+            <h2 className={`${css.homeContainer} ${css.basicFontHome}`} >Just choose <span style={{fontWeight:'bold'}}>Log In</span> or <span style={{fontWeight:'bold'}}>Registration</span> to use your personal <span style={{fontWeight:'bolder'}}>Phone</span><span className={css.bookLogo}>Book</span></h2>
         </div>
     );
 };
