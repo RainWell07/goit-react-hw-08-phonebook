51af058f9ce20acc21eabd9ac32220263223d565
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-07-23 20:07:17 +0300

    Update NotFound.js

--- src/components/Navigation/NotFound.js
+++ src/components/Navigation/NotFound.js
@@ -4,7 +4,7 @@ export default function NotFound() {
     return(
         <div className={css.homeOverlay} style={{marginTop:"100px"}}>
             <h1 className={`${css.homeContainer} ${css.basicFont}`} style={{marginTop:0, marginLeft:"auto", marginRight:"auto"}}>Oops, we have a problem🫢</h1>
-            <h2 className={`${css.homeContainer} ${css.basicFontHome}` } style={{fontSize:'23px'}} >It seems that you`ve navigated to a page that no longer <span style={{fontWeight:'bold'}}>exists<br/></span>Please, navigate to our homepage using the link below - <span style={{fontWeight:'bold'}}><Link to="/">HomePage</Link></span></h2>
+            <h2 className={`${css.homeContainer} ${css.basicFontHome}` } style={{fontSize:'23px'}} >It seems that you`ve navigated to a page that no longer <span style={{fontWeight:'bold'}}>exists<br/></span>Please, navigate to our <span style={{fontWeight:'bold'}}>homepage</span> by using the link below - <span style={{fontWeight:'bold'}}><Link to="/">HomePage</Link></span></h2>
         </div>
     );
 };
